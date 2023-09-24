10087eb91a3c3afc5ec0cefffc5fcfaefdf730cf
Yash <73131357+yashnaravade@users.noreply.github.com>
2023-09-24 20:09:24 +0000

    add product box component in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,9 +24,10 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { ProductsHeaderComponent } from './components/products-header/products-header.component';
 import { FiltersComponent } from './components/filters/filters.component';
+import { ProductBoxComponent } from './components/product-box/product-box.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsHeaderComponent, FiltersComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsHeaderComponent, FiltersComponent, ProductBoxComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
